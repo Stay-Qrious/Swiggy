952a03681defbfd8091c8c1966054d79f62c339b
Stay-Qrious <103578108+Stay-Qrious@users.noreply.github.com>
2023-08-21 16:22:39 +0530

    Add files via upload

---
+++ src/Contact/Contact.js
@@ -0,0 +1,12 @@
+
+
+const Constant = ()=>{
+    return (
+        <>
+        <div className="h-screen flex justify-center items-center font-bold text-3xl bg-sky-100">
+            <div>Contact- @abhi_tripathi085</div>
+        </div>
+        </>
+    )
+}
+export default Constant
